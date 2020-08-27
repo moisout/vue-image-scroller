ed635db9c958964b97dcf19e6709e85c807c9cc5
Maurice Oegerli <maurice@oeger.li>
2020-08-27 15:44:23 +0200

    init

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,31 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <ImageScroller />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ImageScroller from './components/ImageScroller.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    ImageScroller
   }
 }
 </script>
 
 <style lang="scss">
+html,
+body {
+  margin: 0;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin: 0;
 }
 </style>
---
+++ src/components/ImageScroller.vue
@@ -0,0 +1,73 @@
+<template>
+  <div class="container">
+    <div class="image-container" v-for="(images, index) in getDoubleArray" :key="index">
+      <div class="single-image-container">
+        <div class="image"  v-for="(singleImage, i) in images" :key="`${i}img`">
+          <img :src="singleImage" alt />
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      imagesArray: [
+        'https://images.unsplash.com/photo-1598382570334-4ff525e7e4cb?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1351&q=80',
+        'https://images.unsplash.com/photo-1598458697176-a0d727e317c1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1365&q=80',
+        'https://images.unsplash.com/photo-1593642532009-6ba71e22f468?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80',
+        'https://images.unsplash.com/photo-1598432521392-0ce5ff611c85?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=562&q=80'
+      ]
+    }
+  },
+  computed: {
+    getDoubleArray() {
+      const arr = this.imagesArray
+      return [
+        arr,
+        arr
+      ]
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+@mixin randomBg() {
+  background-color: rgb(random(255), random(255), random(255));
+}
+
+.container {
+  height: 100vh;
+  overflow: hidden;
+
+  .image-container {
+    height: 200%;
+    // animation: imagetransition linear infinite 5s;
+
+    .single-image-container {
+      height: 100%;
+      display: flex;
+      flex-direction: column;
+      overflow: hidden;
+
+      .image {
+        img {
+          height: 100%;
+        }
+      }
+    }
+  }
+}
+
+@keyframes imagetransition {
+  0% {
+    transform: translateY(0%);
+  }
+  100% {
+    transform: translateY(-100%);
+  }
+}
+</style>
